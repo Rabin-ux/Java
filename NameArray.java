8c0f68f912ce9c9a6cb3a6fc21cf00771d7f0b6a
Rabin-ux <rabinraj2060@gmail.com>
2026-01-16 15:36:15 +0530

    Day2-second code for name using arry

---
+++ NameArray.java
@@ -0,0 +1,12 @@
+public class NameArray {
+    public static void main(String[] args) {
+        char[] name = {
+            'R','a','b','i','n',' ',
+            'R','a','j',' ',
+            'C','h','a','u','d','h','a','r','y'
+        };
+        for (int i = 0; i < name.length; i++) {
+            System.out.print(name[i]);
+        }
+    }
+}
