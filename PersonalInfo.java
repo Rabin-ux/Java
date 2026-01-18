4f378033deedaf8fa1e62130221ea6bad5058e69
Rabin-ux <rabinraj2060@gmail.com>
2026-01-18 11:11:02 +0530

    Assignment-1(1st)

---
+++ PersonalInfo.java
@@ -0,0 +1,7 @@
+public class PersonalInfo {
+    public static void main(String[] args) {
+        System.out.println("Name: Rabin Raj Chaudhary");
+        System.out.println("Roll No:24070122148");
+        System.out.println("Address: Janakpurdham, Nepal");
+    }
+}
