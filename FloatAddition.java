a432ebcb89b299e377b8f041687dbdc36b783be4
Rabin-ux <rabinraj2060@gmail.com>
2026-01-18 11:15:37 +0530

    Assignment-3(1st)

---
+++ FloatAddition.java
@@ -0,0 +1,11 @@
+public class FloatAddition {
+    static float add(float a, float b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        float x = 6.5f;
+        float y = 25.3f;
+        double result = add(x, y);
+        System.out.println("Addition Result: " + result);
+    }
+}
