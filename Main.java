a4e0f9a45ec73f850a3521a3068bbefdc51e4001
Rabin-ux <rabinraj2060@gmail.com>
2026-01-19 09:40:19 +0530

    Day-3(1st)

---
+++ Main.java
@@ -0,0 +1,20 @@
+class student {
+    String name;
+    int age;
+
+    student(){
+        name = "Rabin";
+        age = 19;
+    }
+void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+public class Main{
+    public static void main(String[] args) {
+        student student1 = new student();
+        student1.display();
+    }
+}
